1d5ceafaa5f5e473f55e46fd11e0f7b184bdc3de
Molik Miah <molik.miah@replgroup.com>
2017-03-02 14:13:50 +0000

    init commit, new xcode project. compatible with macOS 10.10

---
+++ Dash Browser/Dash Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dash Browser
+//
+//  Created by Molik Miah on 02/03/2017.
+//  Copyright © 2017 Dash. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dash Browser/Dash Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dash Browser
+//
+//  Created by Molik Miah on 02/03/2017.
+//  Copyright © 2017 Dash. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
